61ee31bf76ac314613422767cbf2608dc7880e51
learnwithkawsar721 <kawsar721g@gmail.com>
2025-07-09 13:15:18 +0600

    knowledge-cafe figma file clone from programming hero github

---
+++ m40-react-world-toure/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Countries from './components/countries/Countries'
+
+function App() {
+
+  return (
+    <>
+      <Countries />
+      <h1>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
---
+++ m40-react-world-toure/src/components/countries/Countries.jsx
@@ -0,0 +1,10 @@
+
+const Countries = () => {
+    return (
+        <div>
+            <h2>Countries List</h2>
+        </div>
+    );
+};
+
+export default Countries;
